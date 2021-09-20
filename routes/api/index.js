f8faf4c5bd2f959c0e5ef77a0d0196786230f721
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 16:28:43 -0400

    Add Router that manages the routing of requests to /api/categories, /api/products, /api/tags

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
+
+module.exports = router;
