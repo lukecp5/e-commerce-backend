2188688edfa6dd3a347e30d161687aee4fefac07
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 16:20:20 -0400

    Add main server.js file, package.json, and the connection configuration file for Sequelize

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
