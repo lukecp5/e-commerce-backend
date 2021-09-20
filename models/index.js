0940471575440bd01b8570f1433f9b50eb6ec27b
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 17:27:23 -0400

    Build out associations between Product and Tag models. Products can have many tags, and Tags can have many Products

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tags.BelongsToMany(Product, {
+  through:{
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
 
 module.exports = {
   Product,
