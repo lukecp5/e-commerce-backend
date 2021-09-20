45dd64f7ec9d041f161cf73478466eb85a920f27
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 17:05:52 -0400

    Add id, product_name, price, and stock properties(columns) to Product Model(table)

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,48 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+	{
+		// define columns
+		id: {
+			type: dataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_name: {
+			type: dataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: dataTypes.DECIMAL,
+			allowNull: false,
+			validate: {
+				isDecimal: true,
+			},
+		},
+    stock: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    }
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product",
+	}
 );
 
 module.exports = Product;
