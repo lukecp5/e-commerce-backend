142c07c09202baa537921a8c667a209b0e8ece6e
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 21:32:59 -0400

    Add scope and precision to the price attribute of the Product Model so that the full product price is display instead of it being rounded to the nearest integer

--- models/Product.js
+++ models/Product.js
@@ -23,11 +23,11 @@ Product.init({
     allowNull: false,
   },
   price: {
-    type: DataTypes.DECIMAL,
+    type: DataTypes.DECIMAL(10, 2),
     allowNull: false,
     validate: {
       isDecimal: true,
-    },
+    }
   },
   stock: {
     type: DataTypes.INTEGER,
