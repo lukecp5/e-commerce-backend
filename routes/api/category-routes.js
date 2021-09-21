3492110042ae66c2af0cddb266f2510dfd2f744d
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-21 13:09:54 -0400

    Add API route to handle requests for a specific category(by id). Add API route to handle POST requests to add a new category to the database.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,9 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  Category.findAll({
+  await Category.findAll({
     attributes: ["id", "category_name"],
     include: [{
       model: Product,
@@ -19,13 +19,34 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  await Category.findByPk(req.params.id, {
+    attributes: ["id", "category_name"],
+		include: [
+			{
+				model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+			}
+		],
+	})
+  .then((category) => {
+    res.json(category);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+		.then((newCategory) => res.status(200).json(newCategory))
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json(err);
+		});
 });
 
 router.put('/:id', (req, res) => {
