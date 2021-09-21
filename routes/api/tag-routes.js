9bc573bba6041fb44b08989d13f4f2d911a27ee8
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 22:01:25 -0400

    Add PUT API route for updating of tag names based on their primary key i.e. /api/tags/14

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -56,6 +56,18 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
 	// update a tag's name by its `id` value
+	Tag.update(
+		{
+			tag_name: req.body.tag_name,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	).then((tag) => {
+		res.json(tag);
+	});
 });
 
 router.delete("/:id", (req, res) => {
