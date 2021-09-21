d17d911631f51b54940a8c287b3e1c80e6a8158c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-20 21:45:54 -0400

    Add API route to handle the POST requests to /api/tags/ so that a new tag can be created when the user send a POST request with valid JSON containing a tag_name attribute.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,6 +44,15 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
 	// create a new tag
+	Tag.create({
+		tag_name: req.body.tag_name,
+	})
+	.then((tag) => {
+		res.json(tag);
+	})
+	.catch((err) => {
+		res.json(err);
+	})
 });
 
 router.put("/:id", (req, res) => {
